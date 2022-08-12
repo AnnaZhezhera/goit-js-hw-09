4a582a8c86aca28d0b818d20de3c475a585511b0
Anna Zhezhera <ianna22121996@gmail.com>
2022-08-12 22:17:24 +0300

    Task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  btnStart.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    const newColor = getRandomHexColor();
+    document.body.style.backgroundColor = newColor;
+  }, 1000);
+}
+
+function onClickStop() {
+  btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+  console.log('stop');
+}
