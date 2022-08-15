1a413d6cca5c3765a9cdbe9ae18e1d2dd0649dd0
AnnaZhezhera <101525831+AnnaZhezhera@users.noreply.github.com>
2022-08-15 17:59:20 +0000

    Deploying to gh-pages from @ AnnaZhezhera/goit-js-hw-09@846af4987445ddc83676c43381cfe70b91d6f849 🚀

--- 01-color-switcher.2b5048a4.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),timerId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(timerId),console.log("stop")}));
-//# sourceMappingURL=01-color-switcher.2b5048a4.js.map
